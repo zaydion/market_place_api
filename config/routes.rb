b4c168d3a8dd427a4658d0ee8eda183b88d05b62
cartagena <jcartagena93@gmail.com>
2015-11-25 19:34:21 -0500

    added namespace + constraints + path for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 MarketPlaceApi::Application.routes.draw do
-
+  #API definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api'}, path: '/' do
+      #Listing Resources
+  end
 end
