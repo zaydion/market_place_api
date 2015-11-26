2559963f608f72b4b227778b7862f6b3bba5d0d7
cartagena <jcartagena93@gmail.com>
2015-11-25 19:42:06 -0500

    added rspec gem + factory girl + ffaker

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 MarketPlaceApi::Application.routes.draw do
   #API definition
   namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api'}, path: '/' do
+                              constraints: { subdomain: 'api'}, path: '/raco' do
+    scope module:  :v1 do
       #Listing Resources
+    end
   end
 end
