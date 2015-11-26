573775f695496573647e9a765efd2d52a0c62366
cartagena <jcartagena93@gmail.com>
2015-11-25 21:18:39 -0500

    improves the versioning

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,17 @@ module MarketPlaceApi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # Don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.views_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
+
+    config. autoload_paths += %W(\#{config.root}/lib)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+require 'api_constraints'
+
 MarketPlaceApi::Application.routes.draw do
   #API definition
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api'}, path: '/raco' do
-    scope module:  :v1 do
+    scope module:  :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
       #Listing Resources
     end
   end
