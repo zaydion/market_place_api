30f81b5d0ac95b155e5afddde6960fd51f18e05e
cartagena <jcartagena93@gmail.com>
2015-11-27 18:40:35 -0500

    adds destroy method to users controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ MarketPlaceApi::Application.routes.draw do
     scope module:  :v1,
       constraints: ApiConstraints.new(version: 1, default: true) do
       #Listing Resources
-      resources :users, :only => [:show, :create, :update]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end
